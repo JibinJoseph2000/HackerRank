facb58d0125b052e0a73adf54e4bf0c1b4d1311a
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-07-17 21:05:47 +0400

    Add files via upload
    program to find the difference between the triplets and giving points according to the each person using function

---
+++ list.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    p=0
+    q=0
+    ar=[]
+    for i in range(0,3):
+        if(a[i]>0 and a[i]<101):
+            if(b[i]>0 and b[i]<101):
+                if(a[i]>b[i]):
+                    p=p+1
+                if(a[i]<b[i]):
+                    q=q+1
+    ar.append(p)
+    ar.append(q)
+    return ar
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+    result = []
+    result =compareTriplets(a,b)
+    print(result[0],result[1])
+    fptr.write(' '.join(map(str, result)))    
+    fptr.write('\n')
