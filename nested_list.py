219b37e4604a24874950d005a9cbced3f460c8af
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-08-23 10:23:20 +0400

    Add files via upload
    Given the names and marks of n students,print the names of students who got second lowest marks in the subject using list

---
+++ nested_list.py
@@ -0,0 +1,33 @@
+n=int(input())
+list1=[]
+
+for i in range(0,n):
+    temp=[]
+    name=input()
+    grade=float(input())
+    temp.append(name)
+    temp.append(grade)
+    list1.append(temp)
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        if(list1[j][1]>list1[j+1][1]):
+            temp=list1[j]
+            list1[j]=list1[j+1]
+            list1[j+1]=temp
+min=list1[0][1]
+for i in range(0,n):
+    if(list1[i][1]>min):
+        min1=list1[i][1]
+        break 
+list2=[]
+l=0
+for i in range(0,n):
+    if(list1[i][1]==min1):
+        l=l+1
+        list2.append(list1[i][0])
+list2.sort()
+for k in range(0,l):
+        if(list2[k]!='NULL'):
+                print(list2[k])
+        else:
+                break
