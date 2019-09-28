1ba7c1109b142476ddbf05aa06053baa0ee401ca
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-09-28 19:47:19 +0400

    Update and rename nested_list.py to python/nested_list.py

--- python/nested_list.py
+++ python/nested_list.py
@@ -1,6 +1,6 @@
 n=int(input())
 list1=[]
-
+# for loop is used for entering the values in the list
 for i in range(0,n):
     temp=[]
     name=input()
@@ -8,13 +8,14 @@ for i in range(0,n):
     temp.append(name)
     temp.append(grade)
     list1.append(temp)
+# for loop used for sorting the grades 
 for i in range(0,n):
     for j in range(0,n-i-1):
         if(list1[j][1]>list1[j+1][1]):
             temp=list1[j]
             list1[j]=list1[j+1]
             list1[j+1]=temp
-min=list1[0][1]
+min=list1[0][1]      # Initialising the lowest mark
 for i in range(0,n):
     if(list1[i][1]>min):
         min1=list1[i][1]
