5b2da535919f703b4eb38f3b99724feaab46b700
JibinJoseph2000 <49112780+JibinJoseph2000@users.noreply.github.com>
2019-09-28 20:20:38 +0400

    Update and rename string.py to python/string.py

--- python/string.py
+++ python/string.py
@@ -1,9 +1,10 @@
 if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
+    x = int(input())    # The integer is one side of the cuboid
+    y = int(input())    # The integer is one side of the cuboid
+    z = int(input())    # The integer is one side of the cuboid
     n = int(input())
     list1=[]
+# for loops are used to maintain the sequence in the list 
     for i in range(x+1):
         for j in range(y+1):
             for k in range(z+1):
